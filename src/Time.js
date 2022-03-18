8a03c544d6e2e915ff05f0a3113c2265785d6278
Jonas Ermert <ermert.jonas@yahoo.de>
2022-03-18 13:23:13 +0100

    Project completed

---
+++ src/Time.js
@@ -0,0 +1,8 @@
+import React from "react";
+import moment from "moment";
+
+const Time = ({time}) => {
+    const timeString = moment(time).fromNow();
+    return <span className="time">{timeString}</span>
+}
+export default Time;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { ReactDOM } from "react";
+import Time from "./Time";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import 'index.css';
+
+const FileList = ( { files }) => (
+  <table className="file-list">
+    <tbody>{files.map(file => (
+      <FileListItem file={file} key={file.id}/>
+    ))}</tbody>
+  </table>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const FileListItem = ({ file }) => (
+  <tr className="file-list-item">
+    <FileName file={file}/>
+    <CommitMessage commit={file.latestCommit}/>
+    <td className="age">
+      <Time time={file.updated_at} />
+    </td>
+  </tr>
+)
+
+const CommitMessage = ({commit}) => (
+  <td className="commit-message">{commit.message}</td>
+)
+
+const FileName = ({ file }) => (
+  <>
+    <td className="file-icon">
+      <FileIcon file={file}/>
+    </td>
+    <td>{file.name}</td>
+  </>
+)
+
+const FileIcon = ({file}) => {
+  let icon;
+  switch(file.type) {
+    case 'folder':
+      icon = 'fa-folder'
+      break;
+    case 'file':
+      icon = 'fa-file-text-o'
+  }
+
+  return (
+    <i className={`fa ${icon}`}/>
+  )
+}
+
+const data = [
+  {
+    id:1,
+    name: 'src',
+    type: 'folder',
+    updated_at: '2022-03-16 21:24:00',
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+
+  {
+    id:2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: '2022-03-17 15:54:00',
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+
+  {
+    id:3,
+    name: 'README',
+    type: 'file',
+    updated_at: '2022-03-18 20:24:00',
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  }
+]
+
+ReactDOM.render(
+  <FileList files={data} />,
+  document.querySelector("#root")
+);
